205ccd27ee0d2eb4c86d73544b7b59c199c81917
unknown <antoniogf0710@gmail.com>
2013-12-15 04:46:37 -0600

    '15 de diciembre'

--- eulerl33.py
+++ eulerl33.py
@@ -1,29 +1,43 @@
-for i in range(10,19):
-    a=[i for i in str(i)]
-    for j in range(10,19):
-        b=[u for u in str(j)]    
-        x=a[0]
-        y=a[1]
-        w=b[0]
-        z=b[1]
-        p= x==w 
-        q= x==z 
-        r= y==w 
-        s= y==z
-        if y=='0' or x=='0':
-            #print i,j, 'tururu'
+r4=0
+for i in range(10,100):
+    a=[m for m in str(i)]
+    for j in range(10,100):
+        if r4==4:
+            break
+        if i==j:
+            continue
         else:
-            if (p or q or r or s) ==True:
-                if p==True:
-                    if float(int(y))/int(z)==float(i)/j:
-                        print i,j
-                if q==True:
-                    if float(int(y))/int(w)==float(i)/j:
-                        print i,j
-                if r==True:
-                    if float(int(x))/int(z)==float(i)/j:
-                        print i,j
-                if s==True:
-                    if float(int(x))/int(w)==float(i)/j:
-                        print i,j
-    
+            b=[u for u in str(j)]    
+            x=int(a[0])
+            y=int(a[1])
+            w=int(b[0])
+            z=int(b[1])
+            p= x==w 
+            q= x==z 
+            r= y==w 
+            s= y==z
+            if z==0 or w==0:
+                pass
+            else:
+                if (p or q or r or s)==True and (x or y or z or w) !=0:
+                    
+                    if p==True:
+                        if float(y)/int(z)==float(i)/j:
+                            print i,j
+                            r4+=1
+                            continue
+                    if q==True:
+                        if float(y)/int(w)==float(i)/j:
+                            print i,j
+                            r4+=1
+                            continue
+                    if r==True:
+                        if float(x)/int(z)==float(i)/j:
+                            print i,j
+                            r4+=1
+                            continue
+                    if s==True:
+                        if float(x)/int(w)==float(i)/j:
+                            print i,j
+                            r4+=1
+                            continue
